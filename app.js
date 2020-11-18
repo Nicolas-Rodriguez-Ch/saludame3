5390a2ee51c168e951af3e708ecf333382f0257d
Nicolas <nicolasrodriguezch@hotmail.com>
2020-11-17 20:40:14 -0500

    updates app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded());
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
 
 app.get("/", (req, res) => {
-    res.render("index")
-
+    res.render("index");
 });
 
 app.post("/", (req, res) => {
-    res.render("name")
+    res.send(`<h1>Hola ${req.body.name}!</h1>`)
 });
 
 app.listen(3000, ()=> console.log("Listening on port 3000"))
